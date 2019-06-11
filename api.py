f28d21d392782b53a580ae4b753ac1a7da3ea1e1
Mathieu <math.snabre@gmail.com>
2019-06-11 17:13:23 +0200

    api for predict diabete

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import os
 import pandas as pd
 import numpy as np
+from werkzeug.exceptions import BadRequest
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
@@ -42,6 +43,33 @@ lr_model = LogisticRegression(
 lr_model.fit(X_train, y_train.ravel())
 
 
+def args_grouping_required(func):
+    def wraps(*args, **kwargs):
+        if not request.args.get('num_preg'):
+            raise BadRequest({"error": "missing.argument.num_preg"})
+        if not request.args.get('glucose_conc'):
+            raise BadRequest({"error": "missing.argument.glucose_conc"})
+        if not request.args.get('diastolic_bp'):
+            raise BadRequest({"error": "missing.argument.diastolic_bp"})
+        if not request.args.get('thickness'):
+            raise BadRequest({"error": "missing.argument.thickness"})
+        if not request.args.get('insulin'):
+            raise BadRequest({"error": "missing.argument.insulin"})
+        if not request.args.get('bmi'):
+            raise BadRequest({"error": "missing.argument.bmi"})
+        if not request.args.get('diab_pred'):
+            raise BadRequest({"error": "missing.argument.diab_pred"})
+        if not request.args.get('age'):
+            raise BadRequest({"error": "missing.argument.age"})
+        return func(*args, **kwargs)
+    return wraps
+
+
+def accuracy_score():
+    lr_predict_test = lr_model.predict(X_test)
+    return metrics.accuracy_score(y_test, lr_predict_test)
+
+
 @app.route('/')
 def index():
     return "Welcome to Diabetes diagnostic Machine Learnig API"
@@ -51,6 +79,20 @@ def index():
 def testing_modele():
     lr_predict_test = lr_model.predict(X_test)
     return jsonify(accuracy="{0:.4f}".format(metrics.accuracy_score(y_test, lr_predict_test)))
+
+
+@app.route('/predictDiabete')
+@args_grouping_required
+def predict_diabete():
+    data = [[request.args.get('num_preg'), request.args.get(
+        'glucose_conc'), request.args.get('diastolic_bp'), request.args.get('thickness'), request.args.get('insulin'), request.args.get('bmi'), request.args.get('diab_pred'), request.args.get('age')]]
+    df = pd.DataFrame(data, columns=['num_preg', 'glucose_conc', 'diastolic_bp',
+                                     'thickness', 'insulin', 'bmi', 'diab_pred', 'age'])
+    print(df)
+    lr_predict_test = lr_model.predict(df)
+    print(lr_predict_test)
+    res = True if lr_predict_test[0] == 1 else False
+    return jsonify(resultat=res, accuracy=accuracy_score())
 
 
 if __name__ == "__main__":
